ee00589d2112577faba2976b948ba21e7257e95c
ItsOrius <thegameradmin@icloud.com>
2023-01-15 00:49:24 -0500

    fixed wrong calculation

--- src/commands/customRole.js
+++ src/commands/customRole.js
@@ -197,7 +197,7 @@ function execute(client, interaction) {
       // check if icon (variable is a image url, find IMAGE size) is more than 2048 kilobytes
       let downloadedIcon = probe.sync(icon);
       if (downloadedIcon) {
-        if (downloadedIcon.width * downloadedIcon.height > 2048000) {
+        if (downloadedIcon.length > 2048000) {
           // runs if icon is too large
           interaction.reply({ embeds: [quickEmbed("Invalid Icon", "Please use a valid role icon that is under 2048 kilobytes in size!", Discord.Colors.Red)], ephemeral: true }).catch(console.log);
           return;
