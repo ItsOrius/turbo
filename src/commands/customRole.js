4bccf0bf1d7191ec30f3148a76f1975feb8aa307
ItsOrius <thegameradmin@icloud.com>
2023-03-28 13:12:05 -0400

    fixed error with updating roles without an icon

--- src/commands/customRole.js
+++ src/commands/customRole.js
@@ -266,11 +266,13 @@ function execute(client, interaction) {
         return;
       }
       // check if icon (variable is a image url, find IMAGE size) is more than 2048 kilobytes
-      let downloadedIcon = probe.sync(icon);
-      if (icon && (downloadedIcon.length > 2048000 || !downloadedIcon)) {
-        // runs if icon is too large
-        interaction.reply({ embeds: [quickEmbed("Invalid Icon", "Please use a valid role icon that is under 2048 kilobytes in size!", Discord.Colors.Red)], ephemeral: true }).catch(console.log);
-        return;
+      if (icon) {
+        let downloadedIcon = probe.sync(icon);
+        if (downloadedIcon.length > 2048000 || !downloadedIcon) {
+          // runs if icon is too large
+          interaction.reply({ embeds: [quickEmbed("Invalid Icon", "Please use a valid role icon that is under 2048 kilobytes in size!", Discord.Colors.Red)], ephemeral: true }).catch(console.log);
+          return;
+        }
       }
       // send review embed to review channel
       createReviewMessage(interaction, name, color, icon);
